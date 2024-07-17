1f4512677a12c2c20f46f0f7c839d202f4494214
Jonathan Scruggs <30415863+Jonathan-Scruggs@users.noreply.github.com>
2024-07-16 23:42:14 -0600

    Updated Some Files

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
-# React + Vite
-
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+# Features
+- State to keep track of what screen should be displayed
+- Start and Question Screen
+- OTDB API(https://opentdb.com/api_config.php)
+-Tally correct answers and uses color coding to distinguish between correct and incorrect answers.
+- Difficultly,category and question type selector.
+- Tim
--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,19 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+/* ****HINTS******
+  - questions property of API response is html entity. Use a library such as he or html-entities to decode and the HTML entities.
+  
+  - Should create a new array with all answers and randomly insert the correct answers into the array with incorrect answers. Essentially we need to
+  either insert at random or shuffle the items in the array.
 
+  - Limit answer choice to 1:
+    1.) Track the selected answer index inside each question object or 
+    2.) USe a HTML form + radio buttons to allow for one selection.(Have to style it like a button)
+*/
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    
   )
 }
 
